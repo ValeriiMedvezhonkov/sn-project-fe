569fd5e96d0e7c6b678208dff18266df61cabc0b
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-12-04 22:17:03 +0100

    feat: init tailwindcss
    init tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-   return <div>Init project</div>;
+   return (
+      <div>
+         <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      </div>
+   );
 }
 
 export default App;
