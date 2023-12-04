890af786f39d40d62bd62b7b7e49f230cf279361
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-12-04 22:38:44 +0100

    feat: shadcn-ui and @redux-toolkit
    Integrate shadcn-ui components with @redux-toolkit in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
    return (
-      <div>
+      <div className="text-center">
          <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+         <Button>Click me</Button>
       </div>
    );
 }
